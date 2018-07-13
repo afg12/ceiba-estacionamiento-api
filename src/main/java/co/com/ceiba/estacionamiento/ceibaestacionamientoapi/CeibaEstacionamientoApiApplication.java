8f96ad55a3c6fc198c7c641358d2ecec05eec748
Andrea Florez Garcia - Ceiba Software <andrea.florez@ceiba.com.co>
2018-07-13 15:30:13 -0500

    Induccion

---
+++ src/main/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/CeibaEstacionamientoApiApplication.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.estacionamiento.ceibaestacionamientoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaEstacionamientoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaEstacionamientoApiApplication.class, args);
+	}
+}
