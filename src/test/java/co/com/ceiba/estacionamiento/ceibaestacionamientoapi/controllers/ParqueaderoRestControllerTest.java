07f7b258525848250b06a261da5bb087c1115057
Andrea Florez Garcia - Ceiba Software <andrea.florez@ceiba.com.co>
2018-07-23 14:13:11 -0500

    configuracion h2 test

--- src/test/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/controllers/ParqueaderoRestControllerTest.java
+++ src/test/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/controllers/ParqueaderoRestControllerTest.java
@@ -1,72 +1,78 @@
 package co.com.ceiba.estacionamiento.ceibaestacionamientoapi.controllers;
 
-import static org.junit.Assert.fail;
-
-import java.util.Date;
-import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
 import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.CeibaEstacionamientoApiApplication;
-import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.models.entity.Tiquete;
-import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.util.TipoVehiculo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CeibaEstacionamientoApiApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-@TestPropertySource(locations = "classpath:application.properties")
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ParqueaderoRestControllerTest {
 	
-	public static final String REST_SERVICE_URI = "http://localhost:8080/parqueadero";
-	private RestTemplate restTemplate;
+	//public static final String REST_SERVICE_URI = "http://localhost:8080/parqueadero";
+	//private RestTemplate restTemplate;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	//@Autowired
+    //private WebApplicationContext webApplicationContext;
 	
 	 @Before
     public void setUp() {
-        restTemplate = new RestTemplate();
+        //restTemplate = new RestTemplate();
+		 //this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 	 
 	@After
     public void tearDown() {
-        restTemplate = null;
+        //restTemplate = null;
     }
+	
+	@Test
+	public void verifyAllToDoList() throws Exception {
+		mockMvc.perform(get("/parqueadero/listar").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
 
-	/*POST*/
+	/*POST
 	@Test
 	public void isRegistrarEntradaTest() {
 		//arrange
         HttpEntity<Tiquete> request = new HttpEntity<>(new Tiquete("L123", "400", TipoVehiculo.MOTO, new Date(), null, 2000.00));
         
         //act
-        ResponseEntity<Tiquete> response = restTemplate.exchange(REST_SERVICE_URI+"/registrar", HttpMethod.POST, request, Tiquete.class);
+        ResponseEntity<Tiquete> response = restTemplate.exchange("/registrar", HttpMethod.POST, request, Tiquete.class);
         
         //assert
         Assert.assertNotNull(response.getBody());
         Assert.assertEquals(HttpStatus.CREATED.value(),response.getStatusCode().value());
 	}
 	
-	/*POST*/
-	@Test
+	/*POST
+	/*@Test
 	public void noRegistrarEntradaTest() {
 		//arrange
         HttpEntity<Tiquete> request = new HttpEntity<>(new Tiquete("L123", "400", TipoVehiculo.MOTO, new Date(), null, 2000.00));
     	
         try {
         	//act
-        	restTemplate.exchange(REST_SERVICE_URI+"/registrar", HttpMethod.POST, request, Tiquete.class);
+        	restTemplate.exchange("/registrar", HttpMethod.POST, request, Tiquete.class);
         	fail();
         } catch (HttpClientErrorException e) {
         	//assert
@@ -74,26 +80,26 @@ public class ParqueaderoRestControllerTest {
         }
 	}
 	
-	/*PUT*/
+	/*PUT
 	@Test
 	public void facturarTest() {  	
         //act
-        ResponseEntity<Tiquete> response = restTemplate.exchange(REST_SERVICE_URI+"/facturar/1", HttpMethod.PUT, null, Tiquete.class);
+        ResponseEntity<Tiquete> response = restTemplate.exchange("/facturar/1", HttpMethod.PUT, null, Tiquete.class);
         
     	//assert
         Assert.assertNotNull(response.getBody());
         Assert.assertEquals(HttpStatus.OK.value(),response.getStatusCode().value());
 	}
 	
-	/*GET*/
+	/*GET
 	@Test
 	public void listarTiquetesTest() {
         //act
-		ResponseEntity<List<Tiquete>> response = restTemplate.exchange(REST_SERVICE_URI+"/listar", HttpMethod.GET, null, new ParameterizedTypeReference<List<Tiquete>>(){});
+		ResponseEntity<List<Tiquete>> response = restTemplate.exchange("/listar", HttpMethod.GET, null, new ParameterizedTypeReference<List<Tiquete>>(){});
         
     	//assert
         Assert.assertNotNull(response.getBody());
         Assert.assertEquals(HttpStatus.OK.value(),response.getStatusCode().value());
-	}
+	}*/
 	
 }
