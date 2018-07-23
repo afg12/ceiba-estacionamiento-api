498891447187aa57e8d79dd26ff8e9108e843244
Andrea <Andrea@192.168.1.11>
2018-07-22 21:27:45 -0500

    Ajustes test
    Signed-off-by: Andrea <Andrea@192.168.1.11>

--- src/main/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/models/services/IVigilanteService.java
+++ src/main/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/models/services/IVigilanteService.java
@@ -1,10 +1,12 @@
 package co.com.ceiba.estacionamiento.ceibaestacionamientoapi.models.services;
 
+import java.util.Calendar;
+
 import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.util.TipoVehiculo;
 
 public interface IVigilanteService {
 	
 	public boolean validarDisponibilidad(TipoVehiculo tipoVehiculo);
 	
-	public boolean validarPlaca(String placa);
+	public boolean validarPlaca(String placa, Calendar calendar);
 }
