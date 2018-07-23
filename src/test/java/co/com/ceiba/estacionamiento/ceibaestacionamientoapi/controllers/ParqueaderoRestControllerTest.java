88f5d49b880ab0e73caaa143cc220e0667397db5
Andrea Florez Garcia - Ceiba Software <andrea.florez@ceiba.com.co>
2018-07-23 15:19:53 -0500

    integration test

--- src/test/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/controllers/ParqueaderoRestControllerTest.java
+++ src/test/java/co/com/ceiba/estacionamiento/ceibaestacionamientoapi/controllers/ParqueaderoRestControllerTest.java
@@ -1,12 +1,13 @@
 package co.com.ceiba.estacionamiento.ceibaestacionamientoapi.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.After;
+import java.util.Date;
+
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.CeibaEstacionamientoApiApplication;
+import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.models.entity.Tiquete;
+import co.com.ceiba.estacionamiento.ceibaestacionamientoapi.util.TipoVehiculo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CeibaEstacionamientoApiApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -35,11 +40,60 @@ public class ParqueaderoRestControllerTest {
 	private MockMvc mockMvc;
 	
 	@Test
-	public void listarTiquetes() throws Exception {
+	public void listarTiquetesTest() throws Exception {
+		//act
 		MockHttpServletResponse response = mockMvc.perform(get("/parqueadero/listar").accept(MediaType.APPLICATION_JSON))
-			.andDo(print()).andExpect(status().isOk()).andReturn().getResponse();
+			.andDo(print()).andReturn().getResponse();
 		
-		Assert.assertEquals(HttpStatus.OK.value(),response.getStatus());
+		//assert
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
 	}
+	
+	@Test
+	public void isRegistrarEntradaTest() throws Exception {
+		
+		MockHttpServletResponse response = mockMvc.perform(
+                post("/parqueadero/registrar").contentType(MediaType.APPLICATION_JSON).content(
+                		asJsonString(new Tiquete("L123", "400", TipoVehiculo.MOTO, new Date(), null, 0.00))
+                )).andReturn().getResponse();
+		
+		//assert
+		Assert.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+	}
+	
+	@Test
+	public void noRegistrarEntradaTest() throws Exception{
+		
+		MockHttpServletResponse response = mockMvc.perform(
+                post("/parqueadero/registrar").contentType(MediaType.APPLICATION_JSON).content(
+                		asJsonString(new Tiquete("L123", "400", TipoVehiculo.MOTO, new Date(), null, 0.00))
+                )).andDo(print()).andReturn().getResponse();
+		
+		//assert
+		Assert.assertEquals(HttpStatus.NOT_ACCEPTABLE.value(), response.getStatus());
+	}
+	
+	@Test
+	public void facturarTest() throws Exception{
+		//arrange
+		Tiquete tiquete = new Tiquete("L123", "400", TipoVehiculo.MOTO, new Date(), null, 0.00);
+		
+		MockHttpServletResponse response = mockMvc.perform(
+                put("/parqueadero/facturar/{id}", 1L).contentType(MediaType.APPLICATION_JSON).content(
+                		asJsonString(tiquete))).andDo(print()).andReturn().getResponse();
+		
+		//assert
+		Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	
+	private static String asJsonString(final Object obj) {
+	    try {
+	        final ObjectMapper mapper = new ObjectMapper();
+	        final String jsonContent = mapper.writeValueAsString(obj);
+	        return jsonContent;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	} 
 	
 }
